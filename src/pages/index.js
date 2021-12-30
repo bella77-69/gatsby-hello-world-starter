320046bbed67faa246d3440e774a7b7e95db78e7
chantelle mckenzie <mrspasceri@hotmail.com>
2021-12-29 18:17:02 -0800

    Linking between pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
-
+import { Link } from "gatsby"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: "tomato" }}>
+      {" "}
+      <h1>Hello world!</h1>
+     <Link to="/page-2">Page 2</Link>
+    </div>
+  )
 }
---
+++ src/pages/page-2.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Home() {
+    return <div style={{ color: 'tomato' }}>
+        <h1>Welcome to Page 2</h1>
+    
+        <Link to="/">Back</Link>
+    </div>
+}
